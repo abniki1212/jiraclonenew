483029fc1e4230b913b7207c9c01e541f0cd5aeb
abniki1212 <abhishek.u@inventia.in>
2025-07-29 11:06:17 +0530

    Read connection string from config

--- jiraclonenew/Program.cs
+++ jiraclonenew/Program.cs
@@ -6,8 +6,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5432;Database=jiraclonenew;Username=postgres;Password=test1234"));
+    options.UseNpgsql(connectionString));
 
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
